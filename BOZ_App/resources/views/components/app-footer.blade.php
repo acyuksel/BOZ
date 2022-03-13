9469674f1ba7bae3d358db0dbdf6ca95a2e6bca3
Tom Coldenhoff <tom.coldenhoff@outlook.com>
2022-03-13 18:26:37 +0100

    Home page

---
+++ BOZ_App/resources/views/components/app-footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-fountain-blue h-20 mt-10 flex flex-row justify-center items-center">
+    <span>Copyright &copy;<b>{{date('Y')}}</b> Bureau Onbeperkte Zaken - Alle rechten voorbehouden</span>
+</footer>
