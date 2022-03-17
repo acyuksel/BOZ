4ae2f1cb7b341c2aa368a443fea8c8e849c603ce
martienreuvers <mt.reuvers@student.avans.nl>
2022-03-17 18:55:22 +0100

    aanmaken upload image

--- BOZ_App/app/Http/Controllers/Admin/ImageController.php
+++ BOZ_App/app/Http/Controllers/Admin/ImageController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
@@ -8,33 +7,32 @@ use App\Models\Media;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-class VideoController extends Controller
+class ImageController extends Controller
 {
-    public function AddVideo(Request $request)
-    {
+
+    public function AddImage(Request $request){
         $validator = Validator::make($request->all(), [
-            'video' => 'required|file|mimes:mp4,video/mp4',
+            'Image' => 'required|file|mimes:png, jpeg, jpg, Images/png',
             'fileName' => 'required|string',
             'isPublic' => "required|boolean"
         ]);
 
         if ($validator->fails()) return redirect()->withErrors($validator);
 
-        if (FileHandeler::SaveFile($request->get("isPublic"), "videos", $request->get("fileName"), $request->file("video"))) {
-            $validator->errors()->add("video", "Something went wrong with saving you video, please try again.");
+        if (FileHandeler::SaveFile($request->get("isPublic"), "images", $request->get("fileName"), $request->file("image"))) {
+            $validator->errors()->add("image", "Something went wrong with saving your image, please try again.");
             return redirect()->withErrors($validator);
         }
-
-        $Video = new Media();
-        $Video->isPublic = $request->isPublic;
-        $Video->name = $request->fileName;
-        $Video->extension = $request->file("video")->clientExtension();
-        $Video->save();
+        $Image = new Media();
+        $Image->isPublic = $request->isPublic;
+        $Image->name = $request->fileName;
+        $Image->extension = $request->file("video")->clientExtension();
+        $Image->save();
 
         return redirect();
     }
 
-    public function DeleteVideo(Request $request)
+    public function DeleteImage(Request $request)
     {
         $validator = Validator::make($request->all(), [
             "id" => "required|integer|exists:media,id"
