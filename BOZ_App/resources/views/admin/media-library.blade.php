78b5f4e6f2f64543d5a06b9036d6584ad4d4fcf4
lauravdh1 <82824855+lauravdh1@users.noreply.github.com>
2022-03-17 12:49:04 +0100

    added display media in media library
    added display of images, videos and audio files in media library

--- BOZ_App/resources/views/admin/media-library.blade.php
+++ BOZ_App/resources/views/admin/media-library.blade.php
@@ -3,12 +3,35 @@
 @section('title', 'Media')
 
 @section('content')
-<div class="row">
-    <div class="col-md-12">
-        <div class="tile">
-            <div class="tile-body">
-            </div>
-        </div>
+
+<h2>Afbeeldingen</h2>
+@foreach((File::allFiles(public_path('images'))) as $image)
+
+<div class="card" style="width: 15rem; display:inline-block;">
+    <img style="height:10vw;" class="card-img-top" src="{{ asset('images/' . $image->getFilename()) }}" alt="Card image cap">
+    <div class="card-body">
     </div>
-</div>
+  </div>
+  @endforeach
+
+  <h2>Video's</h2>
+  @foreach((File::allFiles(public_path('videos'))) as $video)
+  <div class="card" style="width: 15rem; display:inline-block;">
+    <video style="height:10vw;" src="{{ asset('videos/' . $video->getFilename()) }}" controls></video>
+    <div class="card-body">
+    </div>
+  </div>
+  @endforeach
+
+  <h2>Audio Fragmenten</h2>
+  @foreach((File::allFiles(public_path('audioFragments'))) as $audio)
+  <div class="card" style="width: 15rem; display:inline-block;">
+    <video style="height:10vw;" src="{{ asset('audioFragments/' . $audio->getFilename()) }}" controls></video>
+    <div class="card-body">
+    </div>
+  </div>
+  @endforeach
+
+
+
 @endsection
