ba9cac23e4c4818281d84efb35688242ba41d18a
lauravdh1 <82824855+lauravdh1@users.noreply.github.com>
2022-03-17 18:33:12 +0100

    fixed layout

--- BOZ_App/resources/views/admin/media-library.blade.php
+++ BOZ_App/resources/views/admin/media-library.blade.php
@@ -4,34 +4,32 @@
 
 @section('content')
 
-<h2>Afbeeldingen</h2>
-@foreach((File::allFiles(public_path('images'))) as $image)
-
-<div class="card" style="width: 15rem; display:inline-block;">
-    <img style="height:10vw;" class="card-img-top" src="{{ asset('images/' . $image->getFilename()) }}" alt="Card image cap">
-    <div class="card-body">
+<h4 class="m-2 font-weight-bold text-primary">Afbeeldingen</h4>
+    <div class="m-2 row">
+        @foreach((File::allFiles(public_path('images'))) as $image)
+        <div class="m-1 card" style="width: 15rem;">
+            <img style="height: 15vw; object-fit: cover;" src="{{ asset('images/' . $image->getFilename()) }}" alt="Card image cap">
+        </div>
+        @endforeach
     </div>
-  </div>
-  @endforeach
 
-  <h2>Video's</h2>
-  @foreach((File::allFiles(public_path('videos'))) as $video)
-  <div class="card" style="width: 15rem; display:inline-block;">
-    <video style="height:10vw;" src="{{ asset('videos/' . $video->getFilename()) }}" controls></video>
-    <div class="card-body">
+
+  <h4 class="m-2 font-weight-bold text-primary">Video's</h4>
+    <div class="m-2 row">
+        @foreach((File::allFiles(public_path('videos'))) as $video)
+        <div class="m-2 card" style="width: 15rem;">
+            <video style="height: 10vw;" src="{{ asset('videos/' . $video->getFilename()) }}" controls></video>
+        </div>
+        @endforeach
     </div>
-  </div>
-  @endforeach
 
-  <h2>Audio Fragmenten</h2>
-  @foreach((File::allFiles(public_path('audioFragments'))) as $audio)
-  <div class="card" style="width: 15rem; display:inline-block;">
-    <video style="height:10vw;" src="{{ asset('audioFragments/' . $audio->getFilename()) }}" controls></video>
-    <div class="card-body">
-    </div>
-  </div>
-  @endforeach
-
-
+  <h4 class="m-2 font-weight-bold text-primary">Audio Fragmenten</h4>
+      <div class="m-2 row">
+        @foreach((File::allFiles(public_path('audioFragments'))) as $audio)
+        <div class="m-2 card" style="width: 20rem;">
+            <audio style="height: 4vw;" src="{{ asset('audioFragments/' . $audio->getFilename()) }}" controls></audio>
+        </div>
+        @endforeach
+      </div>
 
 @endsection
