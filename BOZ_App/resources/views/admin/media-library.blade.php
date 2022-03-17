7479feca7d832edd5d37610ff1689e7e5b456f23
lauravdh1 <82824855+lauravdh1@users.noreply.github.com>
2022-03-17 04:48:18 +0100

    added media library page

---
+++ BOZ_App/resources/views/admin/media-library.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.admin.app')
+
+@section('title', 'Media')
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="tile">
+            <div class="tile-body">
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
