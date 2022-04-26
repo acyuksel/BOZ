95330ef49f3a681603df9967f365bfc5ef3c72da
martienreuvers <mt.reuvers@student.avans.nl>
2022-04-26 20:04:45 +0200

    social media buttons in de footer
    social media links naar instagram, facebook en linkedin in de footer.
    
    ik kreeg deze om de een of andere reden niet onder de tekst zonder de hele footer te vervormen dus staan de knoppen er nu naast

---
+++ BOZ_App/resources/views/components/social_media_button.blade.php
@@ -0,0 +1,4 @@
+<a target="_blank" class="px-6 py-2.5 mb-2 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out flex items-center {{$platform}}-button social_media_button " href="{{$link}}">
+   <i class="fa fa-brands fa-{{$platform}}" ></i>&nbsp;{{$platform}}
+   
+</a>
