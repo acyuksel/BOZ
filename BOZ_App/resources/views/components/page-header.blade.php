7484d65fe777834986b5f04c839de11b1dd35c74
Quin-Tempelaars <q.tempelaars@student.avans.nl>
2022-05-10 16:45:47 +0200

    removed the media library open from title homepage

--- BOZ_App/resources/views/components/page-header.blade.php
+++ BOZ_App/resources/views/components/page-header.blade.php
@@ -5,7 +5,7 @@
              src="{{asset($src)}}" alt="">
         <div class="md:min-w-[55%] absolute top-0 h-full px-6 flex flex-col justify-center items-center md:px-0">
             <div class="md:bg-white md:h-3/6 md:flex md:flex-col md:justify-center md:px-6 md:w-full">
-                <h1 id="media-library-open" class="text-4xl font-bold text-center text-white md:text-pickled-bluewood md:text-5xl">{{$title}}</h1>
+                <h1 class="text-4xl font-bold text-center text-white md:text-pickled-bluewood md:text-5xl">{{$title}}</h1>
                 <p class="mt-3 text-xl font-medium text-white md:text-pickled-bluewood md:mt-0">{{$subtitle}}</p>
             </div>
         </div>
@@ -16,7 +16,7 @@
              src="{{asset($src)}}" alt="">
         <div class="absolute top-0 flex flex-col items-center justify-center w-full h-full md:px-0">
             <div class="px-6 md:px-0 md:bg-white md:w-5/6 lg:h-1/6 lg:w-3/6 md:flex md:flex-col md:justify-center md:w-full rounded-xl drop-shadow-xl">
-                <h1 id="media-library-open" class="text-4xl font-bold text-center text-white md:text-black md:text-5xl md:mt-2 lg:mt-0">{{$title}}</h1>
+                <h1 class="text-4xl font-bold text-center text-white md:text-black md:text-5xl md:mt-2 lg:mt-0">{{$title}}</h1>
                 @if($subtitle != '')
                 <p class="text-xl font-medium text-center text-white md:text-black md:mb-2 lg:mb-0 lg:mt-4">{{$subtitle}}</p>
                 @endif
