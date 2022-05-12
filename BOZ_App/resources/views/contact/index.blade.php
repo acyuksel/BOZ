d762900fa1c4b3711df9ab0a66de7eb6906966c7
martienreuvers <mt.reuvers@student.avans.nl>
2022-05-12 15:34:32 +0200

    knoppen aangepast
    knoppen aangepast. in de footer zijn ze klein zonder tekst en op de contact pagina groter met tekst.

--- BOZ_App/resources/views/contact/index.blade.php
+++ BOZ_App/resources/views/contact/index.blade.php
@@ -6,11 +6,6 @@
         <div class="lg:w-container mx-auto mt-5 px-6">
             <h2 class="text-4xl font-medium">{{__('Contact Form')}}</h2>
             <p class="text-lg">{{__('Contact paragraph')}}</p>
-            <div class="w-full flex flex-row justify-center">
-                <x-social_media_button link="https://facebook.com" Platform="facebook" />
-                <x-social_media_button link="https://instagram.com" Platform="instagram" />
-                <x-social_media_button link="https://linkedin.com" Platform="linkedin" />
-            </div>
             <form action="{{route('contact.visitor.store&send')}}" method="post" class="border p-4 rounded mt-5">
                 @csrf
                 @if(isset($success))
@@ -32,6 +27,11 @@
                 </div>
                 <button dusk="submit" type="submit" class="mt-3 drop-shadow-lg bg-paradiso-light text-white text-xl font-bold w-full md:w-48 py-2 rounded-lg hover:scale-105 hover:bg-paradiso-dark selected:scale-105 selected:bg-paradiso-dark transition-all">{{__('Submit')}}</button>
             </form>
+        </div>
+        <div class="w-full flex flex-row justify-center">
+            <x-social_media_button link="https://facebook.com" Platform="facebook" Page="Contact" Size="large" />
+            <x-social_media_button link="https://instagram.com" Platform="instagram" Page="Contact" Size="large"/>
+            <x-social_media_button link="https://linkedin.com" Platform="linkedin" Page="Contact" Size="large" />
         </div>
     </div>
 @endsection
