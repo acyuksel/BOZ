e304909dd06f93e8745d3369f953add536b0b04a
martienreuvers <mt.reuvers@student.avans.nl>
2022-05-27 14:35:43 +0200

    tailwind classes toegevoegd
    zo min mogelijk onnodige css toegevoegd en veel vervangen door tailwind

--- BOZ_App/resources/views/layouts/navigation.blade.php
+++ BOZ_App/resources/views/layouts/navigation.blade.php
@@ -19,15 +19,20 @@
         <nav class="absolute z-10 hidden w-full mt-20 bg-pickled-bluewood md:bg-inherit md:inline md:w-auto md:static md:mt-0 animated--fade-in-down ">
             <ul class="px-6 pb-6 mt-5 md:list-none md:pb-0 md:mt-0 md:flex md:flex-row md:items-center md:h-full">
                 <li class="md:inline-block"><x-nav-link class="" :href="route('home')" :active="request()->routeIs('home')">{{__('Home')}}</x-nav-link></li>
-                <div class="dropdown">
+
+                <div class="dropdown hidden md:relative md:inline-block">
                     <li class="dropbtn mt-3 md:mt-0 md:inline-block md:ml-6"><x-nav-link class="">{{__('About us')}}</x-nav-link></li>
-                    <div class="dropdown-content">
+                    <div class="dropdown-content md:absolute rounded">
                         <li class="mt-3 md:mt-0 md:inline-block md:ml-6"><x-nav-link class="" :href="route('about-us.visitor.index')" :active="request()->routeIs('about-us.visitor.index')">{{__('About us')}}</x-nav-link></li>
                         <li class="mt-3 md:mt-0 md:inline-block md:ml-6"><x-nav-link class="" :href="route('recommendations')" :active="request()->routeIs('recommendations')">{{__('Recommendations')}}</x-nav-link></li>
                         <li class="mt-3 md:mt-0 md:inline-block md:ml-6"><x-nav-link class="" :href="route('partners')" :active="request()->routeIs('partners.visitor.index')">{{__('Samenwerkingspartners')}}</x-nav-link></li>
 
                     </div>
                 </div>
+                <li class="mt-3 md:mt-0 md:hidden md:ml-6"><x-nav-link class="" :href="route('about-us.visitor.index')" :active="request()->routeIs('about-us.visitor.index')">{{__('About us')}}</x-nav-link></li>
+                <li class="mt-3 md:mt-0 md:hidden md:ml-6"><x-nav-link class="" :href="route('recommendations')" :active="request()->routeIs('recommendations')">{{__('Recommendations')}}</x-nav-link></li>
+                <li class="mt-3 md:mt-0 md:hidden md:ml-6"><x-nav-link class="" :href="route('partners')" :active="request()->routeIs('partners.visitor.index')">{{__('Samenwerkingspartners')}}</x-nav-link></li>
+
                 <li class="mt-3 md:mt-0 md:inline-block md:ml-6"><x-nav-link class="" :href="route('projects')" :active="request()->routeIs('projects')">{{__('Projects')}}</x-nav-link></li>
                 <li class="mt-3 md:mt-0 md:inline-block md:ml-6"><x-nav-link class="" :href="route('contact.visitor.index')" :active="request()->routeIs('contact.visitor.index')">{{__('Contact')}}</x-nav-link></li>
                  <li class="mt-3 md:mt-0 md:inline-block md:ml-6"><x-nav-link class="" :href="route('policy.visitor.index')" :active="request()->routeIs('policy.visitor.index')">{{__('Policy')}}</x-nav-link></li>
