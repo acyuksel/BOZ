6ff8dfe6ce85ee6b002cfc24b0aa20428df15b6c
acyuksel <82937921+acyuksel@users.noreply.github.com>
2022-05-30 18:00:07 +0200

    fix

--- BOZ_App/resources/views/components/language-selector.blade.php
+++ BOZ_App/resources/views/components/language-selector.blade.php
@@ -1,12 +1,16 @@
+
 @foreach ($languages as $language)
+@php
+    $activeClasses = ($language->code == "nl" && Cookie::get('app_language') == null) || Cookie::get('app_language') == $language->code?
+    'border-b-8 border-b-pickled-bluewood font-bold' : '';
+@endphp
     <form action="{{ route('set-lang') }}" method="POST" class="px-2 mb-3 mt-2 w-full">
         @csrf
         <input type="hidden" name="lang" value="{{ $language->code }}">
         <button type="submit"
             class="py-3 text-center bg-white rounded  px-4 ease-in w-full border border-slate-200 hover:bg-slate-200
-            {{ Cookie::get('app_language') == $language->code ? 'border-b-8 border-b-pickled-bluewood font-bold' : '' }}">
+            {{ $activeClasses }}">
             {{ $language->name }}
         </button>
     </form>
 @endforeach
-{{--  --}}
