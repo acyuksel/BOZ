2a61ed6f3c62d978308be9a9dabda1f468398baa
Quin-Tempelaars <82822025+Quin-Tempelaars@users.noreply.github.com>
2022-06-21 11:10:50 +0200

    Added cookie dusk tests

--- BOZ_App/resources/views/components/cookie.blade.php
+++ BOZ_App/resources/views/components/cookie.blade.php
@@ -3,8 +3,8 @@
         <div class="flex flex-col">
             <span autofocus>{{ $message }}</span>
             <div class="mt-3">
-                <a href="{{route("cookie.allow")}}" class="px-3 py-1 font-bold text-white bg-green-500 rounded cursor-pointer hover:bg-green-600">{{ __('Ja') }}</a>
-                <a href="{{route("cookie.decline")}}" class="px-3 py-1 font-bold text-white bg-red-500 rounded cursor-pointer hover:bg-red-600">{{ __('Nee') }}</a>
+                <a dusk="CookieAccept" href="{{route("cookie.allow")}}" class="px-3 py-1 font-bold text-white bg-green-500 rounded cursor-pointer hover:bg-green-600">{{ __('Ja') }}</a>
+                <a dusk="CookieDecline" href="{{route("cookie.decline")}}" class="px-3 py-1 font-bold text-white bg-red-500 rounded cursor-pointer hover:bg-red-600">{{ __('Nee') }}</a>
                 <a class="px-3 py-1 font-bold text-white rounded cursor-pointer bg-fountain-blue hover:bg-paradiso-light">{{ __('Privacyverklaring')}}</a>
             </div>
         </div>
