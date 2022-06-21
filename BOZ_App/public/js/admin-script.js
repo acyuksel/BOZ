89c7f5717e3519fd0da9cc745d9119b897eda1fe
Quin-Tempelaars <82822025+Quin-Tempelaars@users.noreply.github.com>
2022-06-21 16:10:26 +0200

    Edited froala backend (#74)

--- BOZ_App/public/js/admin-script.js
+++ BOZ_App/public/js/admin-script.js
@@ -3,15 +3,40 @@ var __webpack_exports__ = {};
 /*!**************************************!*\
   !*** ./resources/js/admin-script.js ***!
   \**************************************/
-var firstContent = new FroalaEditor('#firstContent', {
-  toolbarButtons: ['undo', 'redo', '|', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'outdent', 'indent', 'clearFormatting', 'insertTable', 'html'],
-  quickInsertEnabled: false,
-  placeholderText: 'Schrijf hier de inhoud van het project...'
-});
-var secondContent = new FroalaEditor('#secondContent', {
-  toolbarButtons: ['undo', 'redo', '|', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'outdent', 'indent', 'clearFormatting', 'insertTable', 'html'],
-  quickInsertEnabled: false,
-  placeholderText: 'Schrijf hier een tweede inhoud...'
-});
+var firstContent = createEditor("firstContent");
+var secondContent = createEditor("secondContent");
+
+function createEditor(textAreaId) {
+  var editor = new FroalaEditor('#' + textAreaId, {
+    toolbarButtons: {
+      // Key represents the more button from the toolbar.
+      moreText: {
+        // List of buttons used in the  group.
+        buttons: ['fontSize', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'textColor', 'backgroundColor', 'inlineClass', 'inlineStyle', 'clearFormatting'],
+        // Alignment of the group in the toolbar.
+        align: 'left',
+        // By default, 3 buttons are shown in the main toolbar. The rest of them are available when using the more button.
+        buttonsVisible: 4
+      },
+      moreParagraph: {
+        buttons: ['alignLeft', 'alignCenter', 'formatOLSimple', 'alignRight', 'alignJustify', 'formatOL', 'formatUL', 'paragraphFormat', 'paragraphStyle', 'lineHeight', 'outdent', 'indent', 'quote'],
+        align: 'left',
+        buttonsVisible: 3
+      },
+      moreRich: {
+        buttons: ['insertTable', 'insertLink', 'specialCharacters', 'insertHR'],
+        align: 'left',
+        buttonsVisible: 3
+      },
+      moreMisc: {
+        buttons: ['undo', 'redo', 'html', 'help'],
+        align: 'right',
+        buttonsVisible: 2
+      }
+    },
+    quickInsertEnabled: false
+  });
+  return editor;
+}
 /******/ })()
 ;
