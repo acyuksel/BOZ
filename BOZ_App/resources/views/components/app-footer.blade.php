450de5425c49205b8d6edbb3878b5106ec002e1e
Quin-Tempelaars <q.tempelaars@student.avans.nl>
2022-07-24 13:48:45 +0200

    Added anbi footer

--- BOZ_App/resources/views/components/app-footer.blade.php
+++ BOZ_App/resources/views/components/app-footer.blade.php
@@ -1,14 +1,17 @@
-<footer class="px-6 bg-white mt-10 flex flex-row justify-center items-center overflow-hidden relative">
-    <div class="block bg-fountain-blue h-60 md:w-5 lg:w-10 rotate-45 absolute left-10"></div>
+<footer class="relative flex flex-row items-center justify-center px-6 mt-10 overflow-hidden bg-white">
+    <div class="absolute block rotate-45 bg-fountain-blue h-60 md:w-5 lg:w-10 left-10"></div>
 
-    <div class="block bg-fountain-blue h-60 md:w-5 lg:w-10 rotate-45 absolute md:left-20 lg:left-28"></div>
-    <div class="flex flex-col justify-center mb-4 w-full text-center">
-        <div class="w-full flex md:flex-row items-center justify-center">
-            <x-social_media_button link="https://facebook.com" Platform="facebook" Size="large" />
-            <x-social_media_button link="https://instagram.com" Platform="instagram" Size="large" />
-            <x-social_media_button link="https://linkedin.com" Platform="linkedin" Size="large" />
+    <div class="absolute block rotate-45 bg-fountain-blue h-60 md:w-5 lg:w-10 md:left-20 lg:left-28"></div>
+    
+    <div class="flex flex-col-reverse items-center md:flex-row">
+        <div class="flex flex-col justify-center w-full mb-4 text-center">
+            <div class="flex flex-col items-center justify-center w-full md:flex-row">
+                <x-social_media_button link="https://facebook.com" Platform="facebook" Size="large" />
+                <x-social_media_button link="https://instagram.com" Platform="instagram" Size="large" />
+                <x-social_media_button link="https://linkedin.com" Platform="linkedin" Size="large" />
+            </div>
+            <span>Copyright &copy;<b>{{ date('Y') }}</b> Bureau Onbeperkte Zaken - {{ __('AllRights') }}</span>
         </div>
-        <span>Copyright &copy;<b>{{ date('Y') }}</b> Bureau Onbeperkte Zaken - {{ __('AllRights') }}</span>
+        <img class="w-1/2 m-4 md:w-1/6" src="{{ asset('img/anbi.jpeg') }}"/>
     </div>
-
 </footer>
